5860440e8968f66888d9e6af6b3691e49d6edc1a
edouard modolo <edouardtravers@gmail.com>
2023-11-15 17:01:47 +0100

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,21 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
+
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+macdonald = {name: "MacDonalds", address: "10 rue du gras", phone_number: "01.02.03.04.05", category: "belgian" }
+quick = {name: "Quick", address: "10 rue de l'huile", phone_number: "01.02.03.04.05", category: "belgian" }
+pizza_hut = {name: "Pizza Hut", address: "15 rue de la gerbe", phone_number: "01.02.03.04.05", category: "italian" }
+subway = {name: "Subway", address: "10 rue du pain", phone_number: "01.02.03.04.05", category: "chinese" }
+sushi_shop = {name: "Sushi shop", address: "10 rue du poisson", phone_number: "01.02.03.04.05", category: "japanese" }
+
+[macdonald, quick, pizza_hut, subway, sushi_shop].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
